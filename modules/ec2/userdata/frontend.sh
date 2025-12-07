1006b1c8f6164f2c98ee9fd0efd95920bce77890
ahmedglala <102909097+ahmedglala@users.noreply.github.com>
2025-12-07 03:49:49 +0200

    Update frontend.sh

--- modules/ec2/userdata/frontend.sh
+++ modules/ec2/userdata/frontend.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
-apt update -y
-apt install -y docker.io docker-compose-plugin
+set -e  
+apt-get update -y
+
+apt-get install -y ca-certificates curl gnupg lsb-release
+
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+  https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | \
+  tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update -y
+
+# تثبيت Docker Engine + Compose Plugin
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# إضافة الـ user ubuntu لمجموعة docker عشان يشتغل بدون sudo
 usermod -aG docker ubuntu
+
+# تشغيل Docker service
 systemctl enable docker
+systemctl start docker
+
+echo "Docker + Docker Compose installed successfully!" > /var/log/user-data.log
